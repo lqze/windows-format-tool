e9c5af2afd95febcf689b94ba00aed995b3efb8a
Caleb Fetzer <lqze@users.noreply.github.com>
2016-01-16 22:58:41 +0800

    Update getdrives.py
    added win32_volume_format

--- getdrives.py
+++ getdrives.py
@@ -1,4 +1,5 @@
-import wmi
+import string
+from ctypes import windll
 import win32api, win32file
 
 def get_drives():
@@ -20,32 +21,40 @@ def get_drives():
     #print(drvs)
     '''
     
+    import wmi
+    
     drives = []
 
     c = wmi.WMI ()
     for d in c.Win32_LogicalDisk():
-        if d.FreeSpace:
-            drivestr = '(' + d.Caption + "/" + ') ' + d.VolumeName
-            drives.append(drivestr)
-            '''print( d.Caption, type(d.Size), d.DriveType, d.VolumeName, d.FileSystem)'''
-            infostr = str(int(d.Size) // (1024*1024*1024)) + 'GB   (' + d.FileSystem + ')'
-            print(infostr)
+        if d.Caption != "C:" and d.Caption != "D:":
+            if d.FreeSpace:
+                drivestr = '(' + d.Caption + "/" + ') ' + d.VolumeName
+                drives.append(drivestr)
+                '''print( d.Caption, type(d.Size), d.DriveType, d.VolumeName, d.FileSystem)'''
+                infostr = str(int(d.Size) // (1024*1024*1024)) + 'GB   (' + d.FileSystem + ')'
+                print(infostr)
+                activeDrive = "F:\\"
     
     print(drives)
-    return drives
+    return activeDrive
 	
-def format_drive():
+def format_drive(activeDrive):
 
-'''uint32 Format(
+    '''uint32 Format(
   [in] string FileSystem = "NTFS",
   [in] boolean QuickFormat,
   [in] uint32 ClusterSize = 4096,
   [in] string Label = "",
   [in] boolean EnableCompression = false
-);'''
-    c = wmi.WMI ()
-        for d in c.Win32_LogicalDisk():
-        
+    '''
 
+    print("formatting??")
+    activeDrive.Win32_Volume.Format("FAT32",True,4096,"TEST")
+        
+def main():
+    activdrive = get_drives()
+    format_drive(activdrive)
+        
 if __name__ == '__main__':
-    get_drives() 
+    main() 
