5f7bbce6a354f320873d38b96a6800290a299aa6
Caleb Fetzer <lqze@users.noreply.github.com>
2016-01-17 16:53:52 +0800

    Update getdrives.py

--- getdrives.py
+++ getdrives.py
@@ -1,8 +1,52 @@
-import string
-from ctypes import windll
-import win32api, win32file
+import os, shutil
+import win32gui
+from win32com.shell import shell, shellcon
 
-def get_drives():
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    #copytree recursively copies source directory src to
+    #destination directory dst
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+            
+def choose_firmware_folder():
+    #choose_firmware_folder opens up an interactive file explorer menu 
+    #enabling the user to browse and select the firmware folder
+    desktop_pidl = shell.SHGetFolderLocation(0, shellcon.CSIDL_DESKTOP, 0, 0)
+    pidl, display_name, image_list = shell.SHBrowseForFolder(
+    win32gui.GetDesktopWindow(),
+    desktop_pidl,
+    "Choose a firmware folder",
+    0,
+    None,
+    None
+    )
+
+    firmware_folder = shell.SHGetPathFromIDList(pidl)
+    firmware_folder = firmware_folder.decode()
+    
+    return firmware_folder
+
+    
+def main():
+    src = choose_firmware_folder()
+    os.chdir('F:\\') # choose from volume list getText() from menubox
+    dst = os.getcwd()
+    print("copying...") # add to the logbox
+    copytree(src, dst)
+    print("copied", src, "to", dst) # add to the logbox
+
+    
+if __name__ == "__main__":   
+    main()
+    
+    
     '''drives = []
     bitmask = windll.kernel32.GetLogicalDrives()
     
@@ -20,41 +64,3 @@ def get_drives():
         #drvs.append(drivestr)
     #print(drvs)
     '''
-    
-    import wmi
-    
-    drives = []
-
-    c = wmi.WMI ()
-    for d in c.Win32_LogicalDisk():
-        if d.Caption != "C:" and d.Caption != "D:":
-            if d.FreeSpace:
-                drivestr = '(' + d.Caption + "/" + ') ' + d.VolumeName
-                drives.append(drivestr)
-                '''print( d.Caption, type(d.Size), d.DriveType, d.VolumeName, d.FileSystem)'''
-                infostr = str(int(d.Size) // (1024*1024*1024)) + 'GB   (' + d.FileSystem + ')'
-                print(infostr)
-                activeDrive = "F:\\"
-    
-    print(drives)
-    return activeDrive
-	
-def format_drive(activeDrive):
-
-    '''uint32 Format(
-  [in] string FileSystem = "NTFS",
-  [in] boolean QuickFormat,
-  [in] uint32 ClusterSize = 4096,
-  [in] string Label = "",
-  [in] boolean EnableCompression = false
-    '''
-
-    print("formatting??")
-    activeDrive.Win32_Volume.Format("FAT32",True,4096,"TEST")
-        
-def main():
-    activdrive = get_drives()
-    format_drive(activdrive)
-        
-if __name__ == '__main__':
-    main() 
