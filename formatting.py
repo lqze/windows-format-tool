fc512b80e1187343506380425d6e0f833e855116
Caleb Fetzer <lqze@users.noreply.github.com>
2016-01-18 22:54:25 +0800

    minor changes
    cluster size is default 32K

--- formatting.py
+++ formatting.py
@@ -4,13 +4,16 @@ from ctypes import *
 fm = windll.LoadLibrary('fmifs.dll')  
 
 # relevant fmifs documentation http://doxygen.reactos.org/df/d85/fmifs_8h_source.html  
+# 
 def main():
     test_format = "format F: /Q /V:test"
     print("running command: " + test_format)
     FMT_CB_FUNC = WINFUNCTYPE(c_int, c_int, c_int, c_void_p)
-    FMIFS_HARDDISK = 0x00
-    fm.FormatEx(c_wchar_p('F:\\'), FMIFS_HARDDISK, c_wchar_p('FAT32'),
-    c_wchar_p('hello'), True, c_int(32678), FMT_CB_FUNC(myFmtCallback))
+    #FMIFS_HARDDISK = 0x00
+    FMIFS_UNKNOWN = 0
+    clusterSize = c_int(32768) #leave 32K as cluster size 
+    fm.FormatEx(c_wchar_p('F:\\'), FMIFS_UNKNOWN, c_wchar_p('FAT32'),
+    c_wchar_p('hello'), True, clusterSize, FMT_CB_FUNC(myFmtCallback))
     
     '''
     test_format = "format F: /Q /V:test"
@@ -19,13 +22,12 @@ def main():
     FMIFS_UNKNOWN = 0x00
     fm.FormatEx(c_wchar_p('F:\\'), FMIFS_UNKNOWN, c_wchar_p('FAT32'),
     c_wchar_p('helllollo'), True, c_int(0), FMT_CB_FUNC(myFmtCallback))
-    #32678
     '''
     
     
 
 def myFmtCallback(command, modifier, arg):
-    print(command,modifier,arg)
+    print(command + " " + arg)
     return 1	# TRUE
 
     
