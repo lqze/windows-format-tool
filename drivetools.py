eeb980687cb917ea39db12bdd97b015ced4005ea
lzqe <caleb.fetzer@gmail.com>
2016-01-23 17:46:32 +0800

    various formatting
    formatted the documentation, convention conforming.
    
    modified a check

--- drivetools.py
+++ drivetools.py
@@ -1,56 +1,66 @@
 import os, subprocess, shutil, wmi, win32gui
 from win32com.shell import shell, shellcon
-
+'''
+    copytree recursively copies the source directory src and
+    all of its subdirectories to destination directory dst
+    shutil: NOTE: On Windows, file owners, ACLs and
+    alternate data streams are not copied
+    c.f. https://docs.python.org/3/library/shutil.html
+'''
 def copytree(src, dst, symlinks=False, ignore=None):
-    #copytree recursively copies the source directory src and
-    #all of its subdirectories to destination directory dst
-    #shutil: NOTE: On Windows, file owners, ACLs and 
-    #alternate data streams are not copied
-    #c.f. https://docs.python.org/3/library/shutil.html
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
         if os.path.isdir(s):
             shutil.copytree(s, d, symlinks, ignore)
 
+'''
+    get_drives builds and returns a list of internal / external volumes
+    devices on the system using the WMI module to enable communication
+    between Python and the Windows Management Instrumentation
+
+    returns: drive letter, label, capacity, filesystem (e.g F: AUDIO 10GB NTFS)
+'''
 def get_drives():
-    #get_drives builds and returns a list of internal and external storage
-    #devices on the system using the WMI module to enable communication
-    #between Python and the Windows Management Instrumentation
     drives = []
     c = wmi.WMI().Win32_LogicalDisk()
     for d in c:
-        if d.Caption != "C:": # skip system drive
-            if d.Size or d.DriveType != 5:
-                infostr = "{0:<3} {1:15}  {2:>6} {3:>4}".format(d.Caption+'/', d.VolumeName,
-                    str(int(d.Size) // (1024*1024*1024))+'GB', d.FileSystem)
+        if d.Caption != "C:":       # skip system drive
+            # skip optical disks if DriveType is equal to 5
+            if not d.Size or d.DriveType != 5:
+                infostr = "{0:<3} {1:15}  {2:>6} {3:>4}".format(d.Caption+'/',
+                                                                d.VolumeName,
+                                    str(int(d.Size) // (1024*1024*1024))+'GB',
+                                                                d.FileSystem)
                 drives.append(infostr)
     return drives
-       
+
+'''
+    choose_firmware_folder opens up an interactive file explorer menu
+    enabling the user to browse and select the firmware folder
+'''
 def choose_firmware_folder():
-    #choose_firmware_folder opens up an interactive file explorer menu 
-    #enabling the user to browse and select the firmware folder
     desktop_pidl = shell.SHGetFolderLocation(0, shellcon.CSIDL_DESKTOP, 0, 0)
     pidl, display_name, image_list = shell.SHBrowseForFolder(
-        win32gui.GetDesktopWindow(),
-        desktop_pidl,
-        "Choose a firmware folder",
-        0,
-        None,
-        None
-        )    
+    win32gui.GetDesktopWindow(),
+    desktop_pidl,
+    "Choose a firmware folder", 0, None, None
+    )
     try:
         firmware_folder = shell.SHGetPathFromIDList(pidl)
         firmware_folder = firmware_folder.decode()
     except:
         return ''
-    
+
     return firmware_folder
-    
+
+'''
+    format_disk runs fat32format.exe as a new process and returns its
+    standard output as a string.
+    -c32 sets the allocation unit size to 32K by default
+'''
 def format_disk(drive):
-    #format_disk runs fat32format.exe as a new process and returns its standard output
-    #as a string
-    p = subprocess.Popen('fat32format.exe -c32' + drive,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    p = subprocess.Popen('fat32format.exe -c32' + drive,stdout=subprocess.PIPE,
+                                                        stderr=subprocess.PIPE)
     output_fd1, output_fd2 = p.communicate()
     return output_fd1.decode()
-    
--- guiclass.py
+++ guiclass.py
@@ -8,86 +8,100 @@ class MainApp(Frame):
         Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.initialize()
-    
+
     def initialize(self):
         #create and setup all the UI widgets
-        self.frame = self.create_frame()   
+        self.frame = self.create_frame()
         self.chkbox = self.create_checkbox(self.frame).toggle()
-        self.btn_choose, self.btn_start, self.btn_quit, self.btn_update_list = self.create_btns(self.frame)
-        self.create_static_labels(self.frame, self.btn_update_list)         
+        self.btn_choose, self.btn_start, self.btn_quit, self.btn_update_list \
+                                                = self.create_btns(self.frame)
+        self.create_static_labels(self.frame, self.btn_update_list)
         self.drive_menu = self.select_drive_menu(self.frame)
         self.firmwarebox, self.logbox = self.create_txt_boxes(self.frame)
-        
+
     def create_frame(self):
         root.title("Auto BlackBox")
-        frame = Frame(root,width=300,height=420)
+        frame = Frame(root, width=300, height=420)
         frame.pack()
-        
+
         return frame
-        
+
     def create_static_labels(self, frame, bind_obj):
-        l1 = Label(frame, text="Select Volume")
-        l1.pack()
-        l1.place(relx=0.1, rely=0.05, anchor=NW)
+        lbl_select_volume = Label(frame, text="Select Volume")
+        lbl_select_volume.pack()
+        lbl_select_volume.place(relx=0.1, rely=0.05, anchor=NW)
         l2 = Label(frame, text="")
         l2.pack(side="top")
         l2.place(relx=0.48, rely=0.05, anchor=NW)
         bind_obj.bind("<Enter>", lambda e: l2.configure(text="Refresh drive list"))
         bind_obj.bind("<Leave>", lambda e: l2.configure(text=""))
-        
+
     def create_checkbox(self, frame):
         CheckVar1 = IntVar()
-        C1 = Checkbutton(frame, text = "Quick Format?", variable = CheckVar1, \
-                         onvalue = 1, offvalue = 0, height=1, \
-                         )
+        C1 = Checkbutton(frame, text = "Quick Format?", variable = CheckVar1,
+                                        onvalue = 1, offvalue = 0, height=1, )
         C1.pack()
         C1.place(relx=0.1, rely=0.34, anchor=NW)
         return C1
-  
+
     def start_format(self, drive, firmware_folder):
         output = drivetools.format_disk(drive)
         for line in output:
             self.logbox.insert(INSERT, line)
         self.logbox.insert(INSERT, "\n")
+
+    def copy_folder(self, drive, firmware_folder):
         try:
             self.logbox.insert(INSERT, "Copying folder...\n")
             drivetools.copytree(firmware_folder, drive + '\\')
-            self.logbox.insert(INSERT, 'Successfuly copied {} to location {}\\'.format(firmware_folder, drive))
+            self.logbox.insert(INSERT, 'Successfully copied {} to \
+                                location {}\\'.format(firmware_folder, drive))
         except Exception as e:
             self.logbox.insert(INSERT, "Error copying file. Reason:\n" + str(e))
         print('\a')
-            
+
     def btn_start_callback(self):
-        self.drive_menu = self.select_drive_menu(self.frame)  
+        self.drive_menu = self.select_drive_menu(self.frame)
         self.btn_start.config(state=DISABLED)
         firmware_str = ''.join(self.firmwarebox.get("1.0", END).split('\n'))
+        is_folder = True
         if not firmware_str:
-            tkinter.messagebox.showerror("No Folder Selected" , "Select a firmware location first.")
+            is_folder = tkinter.messagebox.askyesno("""No Folder Selected.
+                            Continue formatting without copying a folder?""")
         else:
             currentDrive = self.drive_menu.get()[0:2]
-            b = tkinter.messagebox.askyesno("Format Disk?" , "Are you sure you wish to format volume {:}\\ ?\nAll data will be erased. This action is irreversible.".format(currentDrive))
+            is_format = tkinter.messagebox.askyesno("Format Disk?" ,
+                            """Are you sure you wish to format volume {:}\\ ?
+                            \nAll data will be erased. This action is
+                                        irreversible.""".format(currentDrive))
             if b:
                 self.logbox.delete(1.0, END)
                 self.start_format(currentDrive, firmware_str)
+                if is_folder:
+                    self.copy_folder(drive, firmware_str)
         self.btn_start.config(state=NORMAL)
-        
+
     def btn_choose_callback(self):
         self.firmwarebox.delete(1.0, END)
         folder = drivetools.choose_firmware_folder()
         self.firmwarebox.insert(INSERT, folder)
-        
+
     def btn_quit_callback(self):
         sys.exit(0)
-        
+
     def btn_update_list_callback(self):
-        self.drive_menu = self.select_drive_menu(self.frame) 
-    
+        self.drive_menu = self.select_drive_menu(self.frame)
+
     def create_btns(self, frame):
-        btn_start = Button(frame, text ="START",width=15,command = self.btn_start_callback) 
-        btn_quit = Button(frame, text ="QUIT",width=15,command = self.btn_quit_callback)
-        btn_choose = Button(frame, text ="Select Firmware",width=32,command = self.btn_choose_callback)
+        btn_start = Button(frame, text ="START", width=15,
+                        command = self.btn_start_callback)
+        btn_quit = Button(frame, text ="QUIT", width=15,
+                        command = self.btn_quit_callback)
+        btn_choose = Button(frame, text ="Select Firmware", width=32,
+                                    command = self.btn_choose_callback)
         self.sicon_img = PhotoImage(file="sicon.png")
-        btn_update_list = Button(frame, text='',width=18,command = self.btn_update_list_callback, image=self.sicon_img)
+        btn_update_list = Button(frame, text='', width=18,
+                command = self.btn_update_list_callback, image=self.sicon_img)
         btn_start.pack()
         btn_start.place(relx=0.1, rely=0.42, anchor=NW)
         btn_quit.pack()
@@ -98,28 +112,28 @@ class MainApp(Frame):
         btn_update_list.place(relx=0.8, rely=0.04, anchor=NW)
 
         return btn_choose, btn_start, btn_quit, btn_update_list
-    
+
     def create_txt_boxes(self, frame):
-        logbox = Text(frame,width=39,height=14, font='-size 8 -family Times')
-        logbox.pack()
-        logbox.place(relx=0.1, rely=0.50, anchor=NW)
-        firmwarebox = Text(frame,width=29,height=1)
-        firmwarebox.pack()
-        firmwarebox.place(relx=0.1, rely=0.28, anchor=NW)
-        
-        return firmwarebox, logbox
-    
+        log_box = Text(frame, width=39, height=14, font='-size 8 -family Arial')
+        log_box.pack()
+        log_box.place(relx=0.1, rely=0.50, anchor=NW)
+        firmware_box = Text(frame, width=29, height=1)
+        firmware_box.pack()
+        firmware_box.place(relx=0.1, rely=0.28, anchor=NW)
+
+        return firmware_box, log_box
+
     def select_drive_menu(self, frame):
         var1 = StringVar()
         drivestrs = drivetools.get_drives()
         var1.set(drivestrs[-1])
-        option1 = OptionMenu(frame, var1, *drivestrs)
-        option1.pack()
-        option1.configure(width=32)
-        option1.place(relx=0.1, rely=0.11, anchor=NW)
-          
+        drive_selection = OptionMenu(frame, var1, *drivestrs)
+        drive_selection.pack()
+        drive_selection.configure(width=32)
+        drive_selection.place(relx=0.1, rely=0.11, anchor=NW)
+
         return var1
- 
+
 if __name__ == "__main__":
     root = Tk()
     root.resizable(width=FALSE, height=FALSE)
